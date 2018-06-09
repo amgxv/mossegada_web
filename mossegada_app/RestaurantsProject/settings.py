2d26b1bca5aeb117dfbada5ff6c6062c9f7012f3
amgxv <32461531+amgxv@users.noreply.github.com>
2018-06-09 14:42:27 +0200

    Update settings.py

--- mossegada_app/RestaurantsProject/settings.py
+++ mossegada_app/RestaurantsProject/settings.py
@@ -190,12 +190,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 GOOGLE_MAPS_API = os.environ.get('GMAPS_API', '')
 
 ##Redis Session
-SESSION_ENGINE = 'redis_sessions.session'
+#SESSION_ENGINE = 'redis_sessions.session'
 
-SESSION_REDIS = {
-    'host': os.environ.get('REDIS_HOST', ''),
-    'port': 6379,
-    'db': 0,
-    'prefix': 'session',
-    'socket_timeout': 1
-}
+#SESSION_REDIS = {
+#    'host': os.environ.get('REDIS_HOST', ''),
+#    'port': 6379,
+#    'db': 0,
+#    'prefix': 'session',
+#    'socket_timeout': 1
+#}
