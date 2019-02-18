491f6f0fd119607cdf54c3454175694a526a1024
amgxv <techxv@protonmail.com>
2019-02-18 17:07:52 +0100

    Testing redis

--- mossegada_app/RestaurantsProject/settings.py
+++ mossegada_app/RestaurantsProject/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'RestaurantsProject.wsgi.application'
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
-REDIS_DB = os.environ.get('REDIS_DB', 1)
+REDIS_DB = int(os.environ.get('REDIS_DB', 1))
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
